5af5e5051272fc76093e3feba9d1b78a505dd41e
SiAn20 <sidneyangellysg@gmail.com>
2025-09-06 00:37:55 -0400

    feat(Aug_28_2025): add solutions for Codeforces Round 1046 problems D, E, F1, and F2

---
+++ Aug_28_2025/D_For_the_Champion.cpp
@@ -0,0 +1,80 @@
+/*
+  author: SiAn20
+  Name: Sidney Angelly Sahonero Garrado
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+
+void solve() {
+  int n, k, dif, sum, min = 2e9, X, Y;
+  cin >> n;
+  vector<pair<int, int>> v(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i].first >> v[i].second;
+  }
+
+  // calculamos la minima suma
+  for (int i = 0; i < n; ++i) {
+    if (min > v[i].first + v[i].second)
+      min = v[i].first + v[i].second;
+  }
+  cout << "? L 1000000000\n";
+  cout.flush();
+  cin >> k;
+  cout << "? L 1000000000\n";
+  cout.flush();
+  cin >> k;
+  cout << "? D 1000000000\n";
+  cout.flush();
+  cin >> k;
+  cout << "? D 1000000000\n";
+  cout.flush();
+  cin >> k;
+
+  sum = (k - min - 4e9) * (-1);
+
+  // calculamos la minima resta
+  min = 2e9;
+  for (int i = 0; i < n; ++i) {
+    if (min > v[i].first - v[i].second)
+      min = v[i].first - v[i].second;
+  }
+  cout << "? U 1000000000\n";
+  cout.flush();
+  cin >> k;
+  cout << "? U 1000000000\n";
+  cout.flush();
+  cin >> k;
+  cout << "? U 1000000000\n";
+  cout.flush();
+  cin >> k;
+  cout << "? U 1000000000\n";
+  cout.flush();
+  cin >> k;
+
+  dif = (k - min - 4e9) * (-1);
+
+  X = (sum + dif) / 2;
+  Y = sum - X;
+  cout << "! " << X << " " << Y << endl;
+  cout.flush();
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+}
+
+/*
+     /\_/\
+    ( o.o )
+     > ^ <
+*/
